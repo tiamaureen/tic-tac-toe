1ee05b1fc997f93d117569b5fc9158aa85cb6a2e
tiamaureen <github@tia.maureen.email>
2022-09-05 23:18:29 -0400

    tweaking functions

--- js/app.js
+++ js/app.js
@@ -19,10 +19,12 @@ let board, turn, winner
 
 const squareEls = document.querySelectorAll('div.square')
 const messageEls = document.getElementById('message') // game status
+const resetBtnEls = document.getElementById('restart')
 
 /*----------------------------- event listeners -----------------------------*/
 
-squareEls.forEach((selectSq) => {selectSq.addEventListener("click", handleClick)})
+squareEls.forEach((select) => {select.addEventListener("click", handleClick)})
+resetBtnEls.addEventListener("click", init)
 
 /*-------------------------------- functions --------------------------------*/
 
@@ -36,54 +38,56 @@ function init(){
 }
 
 function render(){
-  board.forEach(function(square,index){
-    if(square === 1){
-      squareEls[index].textContent = 'x'
-    }else if(square === -1){
-      squareEls[index].textContent = 'o'
-    }else{
-      squareEls[index].textContent = ''
+  board.forEach(function(s,i){
+    if(s === 1){
+      squareEls[i].textContent = 'x'
+    }else if(s === -1){
+      squareEls[i].textContent = 'o'
+    }else if(s === null){
+      squareEls[i].textContent = ''
     }
   })
-  if(winner === null){
-    messageEls.textContent = `it's ${turn === 1 ? 'Player X' : 'Player O'}'s turn.`
-  }else if(winner === 'T'){
-    messageEls.textContent = `y'all tied.`
-  }else if(winner === 1){
-    messageEls.textContent = `x, you are the winner!!!.`
-  }else if(winner === -1){
-    messageEls.textContent = `o, you are the winner!!!.`
+  // if(!winner){
+  //   messageEls.textContent = `it's ${turn === 1 ? 'player x' : 'player o'}'s turn.`
+  // }else if(winner === 1){
+  //   messageEls.textContent = `x, you are the winner!!!.`
+  // }else if(winner === -1){
+  //   messageEls.textContent = `o, you are the winner!!!.`
+  // }else if(winner === 'T'){
+  //   messageEls.textContent = `y'all tied. it's a draw. try again?` 
+  // }
+  switch(winner){
+    case 1:
+      messageEls.textContent = `x, you are the winner!!!.`
+    case -1:
+      messageEls.textContent = `o, you are the winner!!!.`
+    case "T":
+      messageEls.textContent = `y'all tied. it's a draw. try again?` 
+    default:
+    messageEls.textContent = `it's ${turn === 1 ? 'player x' : 'player o'}'s turn.`
   }
 }
 
-function handleClick(evt){ //click event
-  let sqIdx = parseInt(evt.target.id.replace('sq', ''))
-  if (board[sqIdx] !== null){
+function handleClick(e){
+  let sIdx = parseInt(e.target.id.replace('sq', ''))
+  if(board[sIdx] || winner !== null){
     return
   }
-  board[sqIdx] = turn
+  board[sIdx] = turn
   turn *= -1
   render()
-  console.log(board[sqIdx])//
-  }
-
-function getWinner(){
-  let winner = null
-  winningCombos.forEach(function(curCombo,index){
-    if(board[curCombo[0]] && board[curCombo[1]] === board[curCombo[1]] && board[curCombo[2]]){
-      winner = board[combo[0]]
-    }
-  })
-  if(winner){
-    return winner
-  }else if(board.includes('')){
-    return 'T'
-  }
 }
 
-//* step 8: reset game functionality
-// function 
-// 8a. add a reset button to the HTML document
-// 8b. tore the new reset button element in a constant named `resetBtnEl`
-// 8c. attach an event listener to the `resetBtnEl`
-//   - on the `'click'` event it should call the `init` function you created in 3
+function getWinner(){
+  let winner = []
+  winningCombos.forEach(function(c, i){
+    if (board[c[0]] && board[c[0]] === board[c[1]] && board[c[0]] === board[c[2]])
+    winner = board[c[0]]
+  })
+  return winner ? winner : board.includes('') ? null : 'T'
+  render()
+}
+
+function resetGame(){
+  init()
+}
